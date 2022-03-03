53dbd083e68ae6d490f698c10b05588d28e3e59a
hinetabi <binhlthe163537@fpt.edu.vn>
2022-03-03 14:54:18 +0700

    function PRF by LTB

---
+++ Function/1. Chuyen co so/dua ve dang nhi phan.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+int main() {
+	system("cls");
+	//INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
+	int a,b;
+	scanf("%d%d",&a,&b);
+
+
+	// Fixed Do not edit anything here.
+	printf("\nOUTPUT:\n");
+	//@STUDENT: WRITE YOUR OUTPUT HERE:
+	char s1[100] = "hello";
+	char s2[100] = "hello";
+	
+	printf("%s ",s1);
+	printf("%s",s2);
+
+	return 0;
+}
+
